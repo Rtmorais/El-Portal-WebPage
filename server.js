5566a9a64a39511ab3503e4ab5e0fbe1f09a61b7
Rtmorais <rt.morais@hotmail.com>
2020-04-11 03:50:03 -0300

    startind server and backend

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+
+server.set("view engine", "html")
+
+server.use(express.static("public"))
+
+server.use(express.static("assets"))
+
+nunjucks.configure("views", {
+    express: server
+})
+
+server.get("/", function (req, res) {
+    return res.render("index")
+})
+
+server.get("/productos", function (req, res) {
+    return res.render("productos")
+})
+
+server.get("/servicios", function (req, res) {
+    return res.render("servicios")
+})
+
+server.get("/contacto", function (req, res) {
+    return res.render("contacto")
+})
+
+server.listen(5000, function () {
+    console.log("Server ON")
+})
