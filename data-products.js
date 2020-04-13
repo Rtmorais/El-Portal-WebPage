5142e439fad09e79cb7a2b9d1f61e96f0314f8f7
Rtmorais <rt.morais@hotmail.com>
2020-04-13 03:10:26 -0300

    404 and inserting dynamic data with js

---
+++ data-products.js
@@ -0,0 +1,42 @@
+module.exports = [
+	{
+		img         :
+			'https://scontent.faep9-1.fna.fbcdn.net/v/t1.0-9/69816737_3035252506547267_844441899547754496_n.png?_nc_cat=109&_nc_sid=85a577&_nc_eui2=AeEevMqSs6drm3BWnBk6rW1uY8Lon0BZcuwOMh9ewVV8UTSTjSaliGosEW3ctBM02GfvsEdOmZmn-YYUBVSu70QadOJ90M3QRf4_fR1DQdIoIw&_nc_oc=AQnfG16hXhG0BoNseiwS5nnigdxnXS_6VA5DtTwi9LqP3qmUzigxTVnh9Q1KReJbPMw&_nc_ht=scontent.faep9-1.fna&oh=36adb16b3d05c7d1b8c1c5422a205b05&oe=5EA97EFE',
+		tittle      : 'Brennan',
+		description : 'Variedad de Especias Ahumadas y Mostazas'
+	},
+
+	{
+		img         :
+			'https://scontent.faep9-1.fna.fbcdn.net/v/t1.0-9/13925092_1645244965803286_1085452354203286379_n.jpg?_nc_cat=109&_nc_sid=85a577&_nc_eui2=AeGoQ1KWPn1BoWF2kzjBoM2r30-GNWA9hMHa5dKIc-hcSqgSsg5fmwxenkG7ZHCx-crh1qx0lJ_pv9CvU0bQKmBrlzLZtpx_CrNSKMuEKdbNtA&_nc_oc=AQnn6k6BW-amApwHRa6hj7PYZisN9IwaVNq5ZTTiUS6dQM9VXN3vKNUcopy9ubpYXgY&_nc_ht=scontent.faep9-1.fna&oh=c678e7c03500e58e830bcc622c16067f&oe=5EA84F16',
+		tittle      : 'Munini',
+		description : 'Variedad de Pescados'
+	},
+
+	{
+		img         :
+			'https://scontent.faep9-1.fna.fbcdn.net/v/t31.0-8/p960x960/26685688_1596907310355129_8490070644994203649_o.jpg?_nc_cat=1&_nc_sid=85a577&_nc_eui2=AeEFgiEPpjTB-UZFrAR8z_7LkfHnIWvlui1Pc_RbU3YMkQtcyPjcj6zNbj31X775JxtKVTY2cgTgTL3cGwRnqOQNvh77Z4uXSX2sAVeg0xczfg&_nc_oc=AQnA6WG62mg8s8RNSSm18-l1eXRT1AKec0TH2EBs9BMvhnmVKFS7d_n3Ig6OE3yxgUM&_nc_ht=scontent.faep9-1.fna&_nc_tp=6&oh=ef45a0be7db73a7ad5bb9a8fc60b027b&oe=5EA76A4A',
+		tittle      : 'McCain',
+		description : 'Primera calidad en papas congeladas'
+	},
+
+	{
+		img         : 'http://www.eldiariodeturismo.com.ar/wp-content/uploads/2017/08/PALADINI-LOGO-16-9.png',
+		tittle      : 'Paladini',
+		description : 'Variedad de fiambres'
+	},
+
+	{
+		img         :
+			'http://staticf5a.diaadia.info/sites/default/files/styles/landscape_1008_566/public/nota_periodistica/milanesas.jpg',
+		tittle      : 'Milanesas y Supremas El Portal',
+		description : 'Las mejores milanesas caseras de la ciudad'
+	},
+
+	{
+		img         :
+			'https://lh3.googleusercontent.com/proxy/pEycy9P94xTNshIG6ZyOI87BqyMq2RAAe7BPBqvEGPEkP70fj3KdWObERUb1oTTkFt4uUpkmcH_qDHnWVE-IMDKwYLEEPXhXzbu4r_c_feoUz4glic64IqSyq_oMH8lZzA',
+		tittle      : 'Lactear',
+		description : 'Gran variedad de quesos'
+	}
+];
--- server.js
+++ server.js
@@ -1,34 +1,45 @@
-const express = require('express')
-const nunjucks = require('nunjucks')
+const express = require('express');
+const nunjucks = require('nunjucks');
+const productos = require('./data-products');
 
-const server = express()
+const server = express();
 
-server.set("view engine", "njk")
+server.set('view engine', 'njk');
 
-server.use(express.static("public"))
+server.use(express.static('public'));
 
-server.use(express.static("assets"))
+server.use(express.static('assets'));
 
-nunjucks.configure("views", {
-    express: server
-})
+nunjucks.configure('views', {
+	express    : server,
+	autoescape : false,
+	noCache    : true
+});
 
-server.get("/", function (req, res) {
-    return res.render("index")
-})
+server.get('/', function(req, res) {
+	return res.render('index');
+});
 
-server.get("/productos", function (req, res) {
-    return res.render("productos")
-})
+server.get('/productos', function(req, res) {
+	return res.render('productos', { items: productos });
+});
 
-server.get("/servicios", function (req, res) {
-    return res.render("servicios")
-})
+server.get('/servicios', function(req, res) {
+	return res.render('servicios');
+});
 
-server.get("/contacto", function (req, res) {
-    return res.render("contacto")
-})
+server.get('/contacto', function(req, res) {
+	return res.render('contacto');
+});
 
-server.listen(5000, function () {
-    console.log("Server ON")
-})
+server.get('/layout', function(req, res) {
+	return res.render('layout');
+});
+
+server.use(function(req, res) {
+	res.status(404).render('not-found');
+});
+
+server.listen(5000, function() {
+	console.log('Server ON');
+});
